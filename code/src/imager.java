a7dc1a2540a83ef69b44b7b16ea972dde3e9ac3e
Acoliver102 <23olivera@cistercianmail.net>
2022-01-12 17:49:57 -0600

    averagin eqn

--- code/src/imager.java
+++ code/src/imager.java
@@ -7,18 +7,40 @@ public class imager {
     public static void main(String... args) throws Exception {
 
         BufferedImage image = ImageIO.read(
-                new URL("https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png"));
+                new URL("https://andymark-weblinc.netdna-ssl.com/product_images/infinite-recharge-power-cell/61893d825fd31c013af07a10/zoom.jpg?c=1636384130"));
 
         int w = image.getWidth();
         int h = image.getHeight();
 
-        int[] dataBuffInt = image.getRGB(0, 0, w, h, null, 0, w);
+        int[] dataBuffInt = image.getRGB(w/4, h/4, 3*w/4, 3*h/4, null, 0, w);
 
         Color c = new Color(dataBuffInt[100]);
 
-        System.out.println(c.getRed());   // = (dataBuffInt[100] >> 16) & 0xFF
-        System.out.println(c.getGreen()); // = (dataBuffInt[100] >> 8)  & 0xFF
-        System.out.println(c.getBlue());  // = (dataBuffInt[100] >> 0)  & 0xFF
-        System.out.println(c.getAlpha()); // = (dataBuffInt[100] >> 24) & 0xFF
+        int red = 0;
+        int blue = 0;
+        int green = 0;
+        int alpha = 0;
+        Color temp;
+
+        for (int pixel : dataBuffInt) {
+
+            temp = new Color(pixel);
+
+            red += temp.getRed();
+            blue += temp.getBlue();
+            green += temp.getGreen();
+            alpha += temp.getAlpha();
+
+        }
+
+        red /= dataBuffInt.length;
+        blue /= dataBuffInt.length;
+        green /= dataBuffInt.length;
+        alpha /= dataBuffInt.length;
+
+        System.out.println(red);   // = (dataBuffInt[100] >> 16) & 0xFF
+        System.out.println(blue); // = (dataBuffInt[100] >> 8)  & 0xFF
+        System.out.println(green);  // = (dataBuffInt[100] >> 0)  & 0xFF
+        System.out.println(alpha); // = (dataBuffInt[100] >> 24) & 0xFF
     }
 }
